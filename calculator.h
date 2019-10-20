7c1da96a57a4a0993af62887f4496d572ed09edc
Artem Sevruk <artem55660120072013@gmail.com>
2019-10-20 13:06:38 +0300

    Finished Labwork 3

--- calculator.h
+++ calculator.h
@@ -10,6 +10,14 @@ class Calculator
 =======
         int Add (int, int);
         int Mul (double, double);
+// MY CHANGES ACCORDING TO THE LAB3 TASK
+// ADDED DIVISION OPERATION
+// FOR INTEGER AND DOUBLE DATA TYPES
+// int Div(int, int);
+// int Div(double, double);
+// WE HAVE 4 BASIC OPERATIONS
+// AND THIS FILE WAS MODIFIED
+// LAB3 TASK HAS BEEN COMPLETED
 >>>>>>> e0ea21b... add a multiplication operation
 };
 
